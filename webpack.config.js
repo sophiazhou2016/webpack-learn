0c0cff75821cd420ca79764050485768f615a89f
zhourongjing <zhourongjing@ppdai.com>
2020-02-09 19:30:25 +0800

    打包文件名字使用占位符[name],其他的也可以[hash]

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
         'sub': './src/index.js'
     },
     output: {
-        filename: 'bundle.js',
+        filename: '[name].js',
         path: path.resolve(__dirname, 'dist')
     },
     module: {
