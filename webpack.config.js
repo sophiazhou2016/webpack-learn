7ce797e95009a7f14e775acc93c1ab1a61e57ab6
zhourongjing <zhourongjing@ppdai.com>
2020-02-10 23:04:47 +0800

    增加babel-loader-连接了webpack，@babel/core是babel的核心,@babel/preset-env es6->es5语法，@babel/polyfill解析其他的新的东西，以及针对用到的语法转化减少打包代码配置。

--- webpack.config.js
+++ webpack.config.js
@@ -64,6 +64,15 @@ module.exports = {
             use: [
                 'style-loader', 'css-loader', 'postcss-loader'
             ]
+        }, {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+            options: {
+                presets: [['@babel/preset-env', {
+                    useBuiltIns: 'usage'
+                }]]
+            }
         }]
     },
     plugins: [
