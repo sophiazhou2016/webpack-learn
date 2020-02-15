b0920b3c5a06dbcfe13a87ae4568a4ea5d94f6f0
zhourongjing <zhourongjing@ppdai.com>
2020-02-15 15:57:15 +0800

    externals的意思是打包的时候，不要打包某个依赖库

--- library/webpack.config.js
+++ library/webpack.config.js
@@ -3,6 +3,7 @@ const path = require('path');
 module.exports = {
     mode: 'production',
     entry: './src/index.js',
+    externals: ['lodash'],
     output: {
         filename: 'library.js',
         path: path.resolve(__dirname, 'dist'),
