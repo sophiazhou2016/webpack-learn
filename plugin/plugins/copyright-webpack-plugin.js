450835f9d47dd834b23b04f202396cf9cf2ec5e0
zhourongjing <zhourongjing@ppdai.com>
2020-02-17 16:31:01 +0800

    debug  node

--- plugin/plugins/copyright-webpack-plugin.js
+++ plugin/plugins/copyright-webpack-plugin.js
@@ -4,8 +4,12 @@ class CopyrightWebpackPlugin {
     }
 
     apply(compiler) {
+        compiler.hooks.emit.tap('CopyrightWebpackPlugin', (compilation) => {
+            console.log('complie');
+        });
         compiler.hooks.emit.tapAsync('CopyrightWebpackPlugin', (compilation, cb) => {
             // console.log(compilation.assets);
+            debugger;
             compilation.assets['copyright.txt'] = {
                 source: function() {
                     return 'copyright by dell lee';
